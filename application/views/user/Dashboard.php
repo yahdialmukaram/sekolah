85a48ac7d157219b5f68a054b0b0f9d5489f87d0
yahdi <yahdialmukaram03@gmail.com>
2020-05-12 20:59:18 +0700

    foto

--- application/views/user/Dashboard.php
+++ application/views/user/Dashboard.php
@@ -7,9 +7,9 @@
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
           <div class="col-md-8 text-center ftco-animate">
-            <!-- <h1 class="mb-4">Kids Are The Best <span>Explorers In The World</span></h1> -->
-			<!-- <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Read More</a></p> -->
-			<img src="<?= base_url();?>template/images/bg_1.jpg" alt="...">
+            <h1 class="mb-4">Kids Are The Best <span>Explorers In The World</span></h1>
+			<p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Read More</a></p> -->
+			<!-- <img src="<?= base_url();?>template/images/bg_1.jpg" alt="..."> -->
           </div>
         </div>
         </div>
@@ -20,10 +20,9 @@
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
           <div class="col-md-8 text-center ftco-animate">
-            <!-- <h1 class="mb-4">Perfect Learned<span> For Your Child</span></h1> -->
-			<!-- <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Read More</a></p> -->
-			<img src="<?= base_url();?>template/images/bg_2.jpg" alt="...">
-          </div>
+            <h1 class="mb-4">Perfect Learned<span> For Your Child</span></h1>
+			<p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Read More</a></p>
+			</div>
         </div>
         </div>
       </div>
